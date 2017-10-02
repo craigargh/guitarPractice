f656981d730cbbdde86c2e30e96358f0bad7379d
craigr <craigr@moo.com>
2017-10-02 19:43:19 +0100

    Add type hints to position

--- guitarPractice/exercises/position.py
+++ guitarPractice/exercises/position.py
@@ -1,5 +1,5 @@
 class Position:
-    def __init__(self, *, fret, guitar_string, finger, is_highlighted=False):
+    def __init__(self, *, fret: int, guitar_string: int, finger: int, is_highlighted: bool = False):
         self.fret = fret
         self.guitar_string = guitar_string
         self.finger = finger
