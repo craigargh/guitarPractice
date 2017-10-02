019c1cf4e38cac871bc3bdd3c0bf6b81c03a57a5
craigr <craigr@moo.com>
2017-10-02 20:19:40 +0100

    Add Em, F and G chords

--- guitarPractice/exercises/chord_db.py
+++ guitarPractice/exercises/chord_db.py
@@ -6,6 +6,9 @@ def get_major_scale_chords() -> iter:
     return [
         c_major(),
         d_minor(),
+        e_minor(),
+        f_major(),
+        g_major()
     ]
 
 
@@ -30,3 +33,40 @@ def d_minor():
     ]
 
     return Chord('D', 'Minor', positions)
+
+
+def e_minor():
+    positions = [
+        Position(guitar_string=6, is_highlighted=True),
+        Position(guitar_string=5, fret=2, finger=2),
+        Position(guitar_string=4, fret=2, finger=3, is_highlighted=True),
+        Position(guitar_string=3),
+        Position(guitar_string=2),
+        Position(guitar_string=1),
+    ]
+
+    return Chord('E', 'Minor', positions)
+
+
+def f_major():
+    positions = [
+        Position(guitar_string=4, fret=3, finger=3, is_highlighted=True),
+        Position(guitar_string=3, fret=2, finger=2),
+        Position(guitar_string=2, fret=1, finger=1),
+        Position(guitar_string=1, fret=1, finger=1, is_highlighted=True),
+    ]
+
+    return Chord('F', 'Major', positions)
+
+
+def g_major():
+    positions = [
+        Position(guitar_string=6, fret=3, finger=2, is_highlighted=True),
+        Position(guitar_string=5, fret=2, finger=1),
+        Position(guitar_string=4),
+        Position(guitar_string=3),
+        Position(guitar_string=2),
+        Position(guitar_string=1, fret=3, finger=3),
+    ]
+
+    return Chord('G', 'Major', positions)
