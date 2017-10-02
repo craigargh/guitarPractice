e962e81bac9f009fc487980a5777bcdaa2d6ebda
craigr <craigr@moo.com>
2017-10-02 19:40:31 +0100

    Add type hints to chord

--- guitarPractice/exercises/chord.py
+++ guitarPractice/exercises/chord.py
@@ -1,19 +1,22 @@
+from guitarPractice.exercises import position
+
+
 class Chord:
-    def __init__(self, root_note, voicing, positions):
+    def __init__(self, root_note: str, voicing: str, positions: list):
         self.root_note = root_note
         self.voicing = voicing
         self.positions = positions
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.positions)
 
-    def __getitem__(self, item):
+    def __getitem__(self, item) -> position:
         return self.positions[item]
 
-    def __iter__(self):
+    def __iter__(self) -> position:
         for position in self.positions:
             yield position
 
     @property
-    def name(self):
+    def name(self) -> str:
         return '{} {}'.format(self.root_note, self.voicing)
