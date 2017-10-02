5f6acc9cd7f79b2c76df252ed5c7742684fb6f37
craigr <craigr@moo.com>
2017-10-02 19:18:10 +0100

    Add list features to chord

--- guitarPractice/exercises/chord.py
+++ guitarPractice/exercises/chord.py
@@ -2,6 +2,17 @@ class Chord:
     def __init__(self, root_note, voicing, positions):
         self.root_note = root_note
         self.voicing = voicing
+        self.positions = positions
+
+    def __len__(self):
+        return len(self.positions)
+
+    def __getitem__(self, item):
+        return self.positions[item]
+
+    def __iter__(self):
+        for position in self.positions:
+            yield position
 
     @property
     def name(self):
--- tests/chord_test.py
+++ tests/chord_test.py
@@ -18,3 +18,27 @@ class ChordTest(TestCase):
         c_major = Chord('C', 'major', [])
 
         self.assertEqual('C major', c_major.name)
+
+    def test_chord_length_is_set_from_number_of_positions(self):
+        c_major = Chord('C', 'major', [1, 2, 3])
+
+        self.assertEqual(3, len(c_major))
+
+    def test_can_access_chord_positions(self):
+        c_major = Chord('C', 'major', [1, 2, 3])
+
+        self.assertEqual(1, c_major[0])
+        self.assertEqual(2, c_major[1])
+        self.assertEqual(3, c_major[2])
+
+    def test_iterating_over_chord_returns_positions_in_order(self):
+        c_major = Chord('C', 'major', [1, 2, 3])
+
+        results = []
+
+        for value in c_major:
+            results.append(value)
+
+        self.assertEqual(1, results[0])
+        self.assertEqual(2, results[1])
+        self.assertEqual(3, results[2])
