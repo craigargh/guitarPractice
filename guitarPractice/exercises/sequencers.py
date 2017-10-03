c013f2dab636a3aa71ee1627b58f67878c4d0dcf
craigr <craigr@moo.com>
2017-10-03 19:34:08 +0100

    Add ascending sequencer

---
+++ guitarPractice/exercises/sequencers.py
@@ -0,0 +1,35 @@
+from itertools import cycle
+
+
+def ascending_sequence(shape: iter, sequence_length: int = None):
+    sequence_length_is_set = sequence_length is not None and sequence_length > 0
+
+    if sequence_length_is_set and sequence_length <= len(shape):
+        sequence = shorten_shape(shape, sequence_length)
+
+    elif sequence_length_is_set and sequence_length > len(shape):
+        sequence = lengthen_shape(shape, sequence_length)
+    else:
+        sequence = shape[:]
+
+    return sequence
+
+
+def shorten_shape(shape, sequence_length):
+    return shape[:sequence_length]
+
+
+def lengthen_shape(shape, sequence_length):
+    shape_repeater = cycle(shape)
+
+    partial_repeats_length = sequence_length % len(shape)
+    partial_repeats_sequence = shape[-partial_repeats_length:]
+
+    full_repeats_length = sequence_length - partial_repeats_length
+
+    full_repeats_sequence = []
+
+    for _ in range(full_repeats_length):
+        full_repeats_sequence.append(next(shape_repeater))
+
+    return full_repeats_sequence + partial_repeats_sequence
---
+++ tests/test_sequences.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+from guitarPractice.exercises import sequencers
+
+
+class TestSequence(TestCase):
+    def test_ascending_sequence_returns_notes_in_ascending_order(self):
+        note_sequence = sequencers.ascending_sequence([1, 2, 3, 4, 5])
+
+        self.assertEqual(len(note_sequence), 5)
+        self.assertEqual(note_sequence, [1, 2, 3, 4, 5])
+
+    def test_ascending_sequence_can_shorten_sequence_when_length_value_is_set(self):
+        note_sequence = sequencers.ascending_sequence([1, 2, 3, 4, 5], sequence_length=3)
+
+        self.assertEqual(len(note_sequence), 3)
+        self.assertEqual(note_sequence, [1, 2, 3])
+
+    def test_ascending_sequence_will_repeat_notes_if_length_is_greater_than_shape_length(self):
+        note_sequence = sequencers.ascending_sequence([1, 2, 3, 4, 5], sequence_length=8)
+
+        self.assertEqual(len(note_sequence), 8)
+        self.assertEqual(note_sequence, [1, 2, 3, 4, 5, 3, 4, 5])
+
+    def test_ascending_sequence_will_repeat_notes_if_length_is_greater_than_double_shape_length(self):
+        note_sequence = sequencers.ascending_sequence([1, 2, 3, 4, 5], sequence_length=13)
+
+        self.assertEqual(len(note_sequence), 13)
+        self.assertEqual(note_sequence, [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 3, 4, 5])
