5baa74bcf0c05bc7eb95ce9c3c7eb6f89f30aa7e
craigr <craigr@moo.com>
2017-10-07 11:18:45 +0100

    Add transform to exercise builder

--- guitarPractice/exercises/exercise_builder.py
+++ guitarPractice/exercises/exercise_builder.py
@@ -6,12 +6,18 @@ from guitarPractice.exercises.exercise import Exercise
 class ExerciseBuilder:
     def __init__(self):
         self.shapes = None
+        self.transformers = []
 
     def set_shapes(self, shapes):
         if self.shapes is not None:
             raise AttributeError("You can only set the exercise shapes once")
 
         self.shapes = shapes
+        return self
+
+    def transform(self, transformer):
+        self.transformers.append(transformer)
+
         return self
 
     def build(self):
--- guitarPractice/tests/test_exercise_builder.py
+++ guitarPractice/tests/test_exercise_builder.py
@@ -13,25 +13,41 @@ class TestExerciseBuilder(TestCase):
             GuitarShape(positions=[9, 10, 11, 12], root_note='C', voicing='m'),
         ]
 
-    def test_build_returns_an_exercise(self):
-        exercise = ExerciseBuilder() \
-            .set_shapes(self.shapes) \
-            .build()
+    def test_set_shapes_sets_builder_shapes(self):
+        builder = ExerciseBuilder() \
+            .set_shapes(self.shapes)
 
-        self.assertTrue(isinstance(exercise, Exercise))
-
-    def test_set_shapes_sets_shapes_for_exercise(self):
-        exercise = ExerciseBuilder() \
-            .set_shapes(self.shapes) \
-            .build()
-
-        self.assertEqual(exercise.shapes, self.shapes)
+        self.assertEqual(builder.shapes, self.shapes)
 
     def test_set_shapes_can_only_be_called_once(self):
         with self.assertRaises(AttributeError):
             ExerciseBuilder() \
                 .set_shapes(self.shapes) \
                 .set_shapes(self.shapes)
+
+    def test_transform_adds_transformer_to_exercise(self):
+        builder = ExerciseBuilder() \
+            .transform("transformer double")
+
+        self.assertEqual(builder.transformers, ["transformer double"])
+
+    def test_can_add_multiple_transformers_to_exercise(self):
+        builder = ExerciseBuilder() \
+            .transform("a") \
+            .transform("b") \
+            .transform("c") \
+            .transform("d")
+
+        self.assertEqual(builder.transformers, ["a", "b", "c", "d"])
+
+
+class TestExerciseBuilderBuild(TestCase):
+    def setUp(self):
+        self.shapes = [
+            GuitarShape(positions=[1, 2, 3, 4], root_note='A', voicing='m'),
+            GuitarShape(positions=[5, 6, 7, 8], root_note='B', voicing='m'),
+            GuitarShape(positions=[9, 10, 11, 12], root_note='C', voicing='m'),
+        ]
 
     def test_build_combines_shapes_and_sets_sequence(self):
         exercise = ExerciseBuilder() \
@@ -41,3 +57,17 @@ class TestExerciseBuilder(TestCase):
         expected_sequence = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
         self.assertEqual(exercise.sequence, expected_sequence)
+
+    def test_build_returns_an_exercise(self):
+        exercise = ExerciseBuilder() \
+            .set_shapes(self.shapes) \
+            .build()
+
+        self.assertTrue(isinstance(exercise, Exercise))
+
+    def test_build_sets_shapes_for_exercise(self):
+        exercise = ExerciseBuilder() \
+            .set_shapes(self.shapes) \
+            .build()
+
+        self.assertEqual(exercise.shapes, self.shapes)
