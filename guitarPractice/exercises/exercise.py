4eb63e184655fd165648acbbacf620a0105a861d
craigr <craigr@moo.com>
2017-10-07 10:17:14 +0100

    Add exercise object

---
+++ guitarPractice/exercises/exercise.py
@@ -0,0 +1,4 @@
+class Exercise:
+    def __init__(self, shapes, sequence):
+        self.shapes = shapes
+        self.sequence = sequence
---
+++ guitarPractice/tests/test_exercise.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+from guitarPractice.exercises.exercise import Exercise
+
+
+class TestExercise(TestCase):
+    def test_exercise_shapes_are_set(self):
+        exercise = Exercise(shapes=[1, 2, 3, 4], sequence=['a', 'b', 'c', 'd'])
+
+        self.assertEqual(exercise.shapes, [1, 2, 3, 4])
+
+    def test_exercise_sequence_is_set(self):
+        exercise = Exercise(shapes=[1, 2, 3, 4], sequence=['a', 'b', 'c', 'd'])
+
+        self.assertEqual(exercise.sequence, ['a', 'b', 'c', 'd'])
