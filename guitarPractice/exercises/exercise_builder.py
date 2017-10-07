1b4edc052b81d9a059d40ffc1387e50cfdecf951
craigr <craigr@moo.com>
2017-10-07 10:34:29 +0100

    Add set_shapes to exercise builder

--- guitarPractice/exercises/exercise_builder.py
+++ guitarPractice/exercises/exercise_builder.py
@@ -2,5 +2,15 @@ from guitarPractice.exercises.exercise import Exercise
 
 
 class ExerciseBuilder:
+    def __init__(self):
+        self.shapes = None
+
+    def set_shapes(self, shapes):
+        if self.shapes is not None:
+            raise AttributeError("You can only set the exercise shapes once")
+
+        self.shapes = shapes
+        return self
+
     def build(self):
-        return Exercise(shapes=[], sequence=[])
+        return Exercise(shapes=self.shapes, sequence=[])
--- guitarPractice/tests/test_exercise_builder.py
+++ guitarPractice/tests/test_exercise_builder.py
@@ -6,7 +6,20 @@ from guitarPractice.exercises.exercise_builder import ExerciseBuilder
 
 class TestExerciseBuilder(TestCase):
     def test_build_returns_an_exercise(self):
-        builder = ExerciseBuilder() \
+        exercise = ExerciseBuilder() \
             .build()
 
-        self.assertTrue(isinstance(builder, Exercise))
+        self.assertTrue(isinstance(exercise, Exercise))
+
+    def test_set_shapes_sets_shapes_for_exercise(self):
+        exercise = ExerciseBuilder() \
+            .set_shapes([1, 2, 3, 4]) \
+            .build()
+
+        self.assertEqual(exercise.shapes, [1, 2, 3, 4])
+
+    def test_set_shapes_can_only_be_called_once(self):
+        with self.assertRaises(AttributeError):
+            ExerciseBuilder() \
+                .set_shapes([1, 2, 3, 4]) \
+                .set_shapes([4, 3, 2, 1])
