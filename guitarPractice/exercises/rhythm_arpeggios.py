d1b331551aacc94a90765ea55b7be0753c4d68d0
Craig Richardson <craigargh@gmail.com>
2017-10-08 19:35:59 +0100

    Added ascending and descending transformer to arpeggio exercise

--- guitarPractice/exercises/rhythm_arpeggios.py
+++ guitarPractice/exercises/rhythm_arpeggios.py
@@ -2,7 +2,8 @@ from functools import partial
 from random import sample, choice
 
 from guitarPractice.exercise_builder.exercise_builder import ExerciseBuilder
-from guitarPractice.exercise_builder.pick_order_transformers import ascending_transformer
+from guitarPractice.exercise_builder.pick_order_transformers import ascending_transformer, \
+    ascending_and_descending_transformer
 from guitarPractice.guitar_shapes.chord_collections import c_major_scale_triad_chords
 
 
@@ -25,7 +26,8 @@ def get_chords(quantity):
 
 def get_level_one_transformer():
     all_transformers = [
-        ascending_transformer,
+        ascending_transformer
+        ascending_and_descending_transformer,
     ]
 
     transformer = choice(all_transformers)
