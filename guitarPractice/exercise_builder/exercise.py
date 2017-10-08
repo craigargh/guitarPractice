eb6d35e2683253b5f3943f1fd38a63e946af7db1
Craig Richardson <craigargh@gmail.com>
2017-10-08 12:54:12 +0100

    Added rudimentary exercise tab

--- guitarPractice/exercise_builder/exercise.py
+++ guitarPractice/exercise_builder/exercise.py
@@ -2,3 +2,29 @@ class Exercise:
     def __init__(self, shapes, sequence):
         self.shapes = shapes
         self.sequence = sequence
+
+    def __str__(self):
+        sorted_sequence = sorted(self.sequence, key=lambda x: x.order)
+
+        unique_order_indexes = set(position.order for position in sorted_sequence)
+
+        tab_list = []
+        for guitar_string in range(1, 7):
+            guitar_string_tab = []
+
+            for beat in range(len(unique_order_indexes)):
+                tab_value = make_beat_tab(sorted_sequence, beat, guitar_string)
+                guitar_string_tab.append(tab_value)
+
+            tab_list.append("".join(guitar_string_tab))
+
+        return "\n".join(tab_list)
+
+
+def make_beat_tab(sequence, beat, guitar_string):
+    found_value = "--"
+    for item in sequence:
+        if item.order == beat and item.guitar_string == guitar_string:
+            found_value = f"{item.fret}-"
+
+    return found_value
--- guitarPractice/tests/test_exercise.py
+++ guitarPractice/tests/test_exercise.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 from guitarPractice.exercise_builder.exercise import Exercise
+from guitarPractice.guitar_shapes.position import Position
 
 
 class TestExercise(TestCase):
@@ -13,3 +14,41 @@ class TestExercise(TestCase):
         exercise = Exercise(shapes=[1, 2, 3, 4], sequence=['a', 'b', 'c', 'd'])
 
         self.assertEqual(exercise.sequence, ['a', 'b', 'c', 'd'])
+
+    def test_str_returns_sequence_as_tabs(self):
+        sequence = [
+            Position(guitar_string=1, order=1),
+            Position(guitar_string=2, order=0),
+            Position(guitar_string=3, order=2),
+        ]
+
+        exercise = Exercise(shapes=[1, 2, 3, 4], sequence=sequence)
+
+        expected_tab = \
+            "--0---\n" \
+            "0-----\n" \
+            "----0-\n" \
+            "------\n" \
+            "------\n" \
+            "------"
+
+        self.assertEqual(expected_tab, str(exercise))
+
+    def test_str_returns_chord_sequence_as_tabs(self):
+        sequence = [
+            Position(guitar_string=1, order=1),
+            Position(guitar_string=2, order=0),
+            Position(guitar_string=3, order=1),
+        ]
+
+        exercise = Exercise(shapes=[1, 2, 3, 4], sequence=sequence)
+
+        expected_tab = \
+            "--0-\n" \
+            "0---\n" \
+            "--0-\n" \
+            "----\n" \
+            "----\n" \
+            "----"
+
+        self.assertEqual(expected_tab, str(exercise))
