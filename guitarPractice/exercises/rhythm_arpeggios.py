3082e32b14e41b671759f4dd3f34d1fe00ab5773
Craig Richardson <craigargh@gmail.com>
2017-10-08 10:30:54 +0100

    Create rhythm arpeggio level one exercise

---
+++ guitarPractice/exercises/rhythm_arpeggios.py
@@ -0,0 +1,36 @@
+from functools import partial
+from random import sample, choice
+
+from guitarPractice.exercise_builder.exercise_builder import ExerciseBuilder
+from guitarPractice.exercise_builder.pick_order_transformers import ascending_transformer
+from guitarPractice.guitar_shapes.chord_collections import c_major_scale_triad_chords
+
+
+def level_one():
+    chords = get_chords(2)
+    transformer = get_level_one_transformer()
+
+    exercise = ExerciseBuilder() \
+        .set_shapes(chords) \
+        .transform(transformer) \
+        .build()
+
+    print(len(exercise.sequence))
+
+
+def get_chords(quantity):
+    all_chords = c_major_scale_triad_chords()
+    return sample(all_chords, quantity)
+
+
+def get_level_one_transformer():
+    notes_per_arpeggio = choice([4, 6])
+
+    all_transformers = [
+        partial(ascending_transformer, sequence_length=notes_per_arpeggio)
+    ]
+
+    return choice(all_transformers)
+
+
+level_one()
