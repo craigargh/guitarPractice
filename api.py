1d7eeb19a3bddd217016869dd6ff89baa05b0b1a
Craig Richardson <craigargh@gmail.com>
2017-10-12 21:28:01 +0100

    Add basic api for exercise json

---
+++ api.py
@@ -0,0 +1,24 @@
+import jsonpickle
+from flask import Flask, jsonify
+
+from guitarPractice.exercises.rhythm_arpeggios import level_one
+
+app = Flask(__name__)
+
+
+@app.route('/exercise/<exercise_name>/<difficulty>', strict_slashes=False)
+def hello_world(exercise_name, difficulty):
+    exercise = level_one()
+    exercise_json = jsonpickle.encode(exercise, unpicklable=False)
+
+    response = app.response_class(
+        response=exercise_json,
+        status=200,
+        mimetype='application/json'
+    )
+
+    return response
+
+
+if __name__ == '__main__':
+    app.run()
