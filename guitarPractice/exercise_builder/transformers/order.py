dd71062f4d4f2e9ed7236046dfcb263d01e25182
Craig Richardson <craigargh@gmail.com>
2017-10-15 19:50:41 +0100

    Refactor transformers

--- guitarPractice/exercise_builder/transformers/order.py
+++ guitarPractice/exercise_builder/transformers/order.py
@@ -13,39 +13,37 @@ def ascending(positions: List[Position], sequence_length: int = None):
 def asc_and_desc(positions: List[Position], sequence_length: int = None):
     asc_length, desc_length = calculate_asc_desc_sequence_length(positions, sequence_length)
 
-    ascending_sequence = positions[:asc_length]
-    descending_sequence = list(reversed(positions[-desc_length:]))
+    asc_sequence = positions[:asc_length]
+    desc_sequence = list(reversed(positions[-desc_length:]))
 
-    sequence = ascending_sequence + descending_sequence
+    sequence = asc_sequence + desc_sequence
 
     return resize_sequence(sequence, sequence_length)
 
 
 def calculate_asc_desc_sequence_length(positions, sequence_length):
-    if sequence_length:
-        asc_length = int(sequence_length / 2)
-        desc_length = asc_length
+    if sequence_length is None:
+        sequence_length = (len(positions) * 2) - 2
 
-        is_even_split = sequence_length % 2 == 0
+    asc_length = int(sequence_length / 2)
+    desc_length = asc_length
 
-        if not is_even_split:
-            desc_length += 1
-    else:
-        asc_length = int(((len(positions) * 2) - 2) / 2)
-        desc_length = asc_length
+    is_even_split = sequence_length % 2 == 0
+
+    if not is_even_split:
+        desc_length += 1
 
     return asc_length, desc_length
 
 
 def ascending_skip(positions: List[Position], sequence_length: int = None):
+    if sequence_length is None:
+        sequence_length = len(positions)
+
     first_note = [positions.pop(0)]
+    last_notes_length = sequence_length - 1
 
-    if sequence_length:
-        last_notes_length = sequence_length - 1
-    else:
-        last_notes_length = len(positions)
-
-    if sequence_length and len(positions) > last_notes_length:
+    if len(positions) > last_notes_length:
         last_notes = positions[-last_notes_length:]
     else:
         last_notes = resize_sequence(positions, last_notes_length)
@@ -54,18 +52,19 @@ def ascending_skip(positions: List[Position], sequence_length: int = None):
 
 
 def asc_and_desc_skip(positions: List[Position], sequence_length: int = None):
-    if not sequence_length or len(positions) <= sequence_length / 2:
+    if sequence_length is None:
+        sequence_length = (len(positions) * 2) - 2
+
+    half_sequence_length = int(sequence_length / 2)
+
+    if len(positions) <= half_sequence_length:
         return asc_and_desc(positions, sequence_length)
 
     first_note = [positions.pop(0)]
-    sequence = positions[:]
 
-    half_sequence_length = sequence_length / 2
+    if len(positions) > half_sequence_length:
+        positions = positions[-half_sequence_length:]
 
-    if len(sequence) > int(half_sequence_length):
-        last_notes_length = int(half_sequence_length)
-        sequence = sequence[-last_notes_length:]
-
-    last_notes = asc_and_desc(sequence, sequence_length - 1)
+    last_notes = asc_and_desc(positions, sequence_length - 1)
 
     return first_note + last_notes
