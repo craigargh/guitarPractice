f9e5ca2bb1817718b5ba3cbef376d946e044ca25
Craig Richardson <craigargh@gmail.com>
2017-10-15 20:13:43 +0100

    Refactor random order transformers

--- guitarPractice/exercise_builder/transformers/random_order.py
+++ guitarPractice/exercise_builder/transformers/random_order.py
@@ -1,70 +1,48 @@
 import random
+from functools import partial
 
 from guitarPractice.exercise_builder.transformers.transformer_utils import resize_sequence
 
 
 def consistent_steps(length):
     order = create_random_order(length)
-
-    def specific_sequence_transformer(positions, sequence_length=None):
-        if sequence_length is None:
-            sequence_length = len(positions)
-
-        notes = get_shapes_in_specific_order(positions, order)
-        return resize_sequence(notes, sequence_length)
-
-    return specific_sequence_transformer
+    return partial(specific_sequence_transformer, order=order)
 
 
 def root_and_consistent_steps(length):
     order = create_random_order(length - 1)
-
-    def specific_sequence_transformer(positions, sequence_length=None):
-        if sequence_length is None:
-            sequence_length = len(positions)
-
-        first_note = [positions.pop(0)]
-        random_notes = get_shapes_in_specific_order(positions, order)
-
-        return resize_sequence(first_note + random_notes, sequence_length)
-
-    return specific_sequence_transformer
+    return partial(specific_sequence_transformer, order=order, pop_first=True)
 
 
 def consistent_strings(length):
     order = create_random_order(length)
-
-    def specific_sequence_transformer(positions, sequence_length=None):
-        if sequence_length is None:
-            sequence_length = len(positions)
-
-        notes = get_shapes_in_specific_order(positions, order, -1)
-
-        return resize_sequence(notes, sequence_length)
-
-    return specific_sequence_transformer
+    return partial(specific_sequence_transformer, order=order, index_multiplier=-1)
 
 
 def root_and_consistent_strings(length):
     order = create_random_order(length - 1)
-
-    def specific_sequence_transformer(positions, sequence_length=None):
-        if sequence_length is None:
-            sequence_length = len(positions)
-
-        first_note = [positions.pop(0)]
-        random_notes = get_shapes_in_specific_order(positions, order, -1)
-
-        return resize_sequence(first_note + random_notes, sequence_length)
-
-    return specific_sequence_transformer
+    return partial(specific_sequence_transformer, order=order, pop_first=True, index_multiplier=-1)
 
 
 def create_random_order(length):
     return random.sample(range(length), length)
 
 
-def get_shapes_in_specific_order(positions, order, index_multiplier=1):
+def specific_sequence_transformer(positions, sequence_length=None, order=None, pop_first=False, index_multiplier=1):
+    if sequence_length is None:
+        sequence_length = len(positions)
+
+    if pop_first:
+        first_note = [positions.pop(0)]
+    else:
+        first_note = []
+
+    random_notes = get_shapes_in_specific_order(positions, order, index_multiplier)
+
+    return resize_sequence(first_note + random_notes, sequence_length)
+
+
+def get_shapes_in_specific_order(positions, order, index_multiplier):
     random_notes = []
 
     for index in order:
