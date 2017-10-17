56e0e76a2e076b5bb7915768c64771a3a779ca60
Craig Richardson <craigargh@gmail.com>
2017-10-17 20:54:52 +0100

    Add API tests

--- guitarPractice/tests/api_tests/test_exercise_api.py
+++ guitarPractice/tests/api_tests/test_exercise_api.py
@@ -9,18 +9,18 @@ class TestExerciseApi(TestCase):
     def setUp(self):
         self.api = app.test_client()
 
-    def test_get_rhythm_arpeggio_one_exercise_returns_ok(self):
+    def test_get_arpeggio_picking_one_exercise_returns_ok(self):
         response = self.api.get('/exercise/arpeggio-picking/1/')
 
         self.assertEqual(response.status_code, 200)
 
-    def test_get_rhythm_arpeggio_one_contains_two_shapes(self):
+    def test_get_arpeggio_picking_one_contains_two_shapes(self):
         response = self.api.get('/exercise/arpeggio-picking/1/')
         data = json.loads(response.data)
 
         self.assertEqual(len(data['shapes']), 2)
 
-    def test_get_rhythm_arpeggio_one_contains_sequence_items(self):
+    def test_get_arpeggio_picking_one_contains_sequence_items(self):
         response = self.api.get('/exercise/arpeggio-picking/1/')
         data = json.loads(response.data)
 
@@ -30,3 +30,27 @@ class TestExerciseApi(TestCase):
         response = self.api.get('/exercise/spooky-skeletons/1/')
 
         self.assertEqual(response.status_code, 404)
+
+    def test_get_arpeggio_picking_two_exercise_returns_ok(self):
+        response = self.api.get('/exercise/arpeggio-picking/2/')
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_arpeggio_picking_two_has_shapes_and_sequence(self):
+        response = self.api.get('/exercise/arpeggio-picking/2/')
+        data = json.loads(response.data)
+
+        self.assertIsNotNone(data['sequence'])
+        self.assertIsNotNone(data['shapes'])
+
+    def test_get_chords_changes_one_exercise_returns_ok(self):
+        response = self.api.get('/exercise/chord-changes/1/')
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_chords_changes_one_has_shapes_and_sequence(self):
+        response = self.api.get('/exercise/chord-changes/1/')
+        data = json.loads(response.data)
+
+        self.assertIsNotNone(data['sequence'])
+        self.assertIsNotNone(data['shapes'])
