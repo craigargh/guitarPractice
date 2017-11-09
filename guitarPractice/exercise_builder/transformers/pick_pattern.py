06d8537976c64ee988af2143b8e36127b8dfa22e
Craig Richardson <craigargh@gmail.com>
2017-11-09 21:29:07 +0000

    Add root note and alternate root note

--- guitarPractice/exercise_builder/transformers/pick_pattern.py
+++ guitarPractice/exercise_builder/transformers/pick_pattern.py
@@ -9,3 +9,35 @@ def get_guitar_string_note(guitar_shape, guitar_string):
         raise ValueError('String number not found in shape')
 
     return positions[0]
+
+
+def get_root_note(guitar_shape):
+    root_guitar_string = get_lowest_string(guitar_shape)
+    return get_guitar_string_note(guitar_shape, root_guitar_string)
+
+
+def get_alternate_root_note(guitar_shape):
+    root_guitar_string = get_lowest_string(guitar_shape)
+
+    found = False
+    possible_strings = range(1, root_guitar_string)
+
+    for possible_string in reversed(possible_strings):
+        try:
+            alternate_root = get_guitar_string_note(guitar_shape, possible_string)
+            found = True
+            break
+        except ValueError:
+            pass
+
+    if not found:
+        raise ValueError('Could not find alternate root note')
+
+    return alternate_root
+
+
+def get_lowest_string(guitar_shape):
+    return max(
+        position.guitar_string
+        for position in guitar_shape.positions
+    )
--- guitarPractice/tests/unit_tests/test_pick_pattern.py
+++ guitarPractice/tests/unit_tests/test_pick_pattern.py
@@ -1,7 +1,6 @@
 from unittest import TestCase
 
 from guitarPractice.exercise_builder.transformers import pick_pattern
-from guitarPractice.guitar_shapes.chord_shapes import g_major
 from guitarPractice.guitar_shapes.guitar_shape import GuitarShape
 from guitarPractice.guitar_shapes.position import Position
 
@@ -19,48 +18,71 @@ class TestPickPattern(TestCase):
 
         self.six_string_shape = GuitarShape('G', 'Major', positions)
 
+        positions_without_string_5 = [
+            Position(guitar_string=6, fret=3, finger=2, is_highlighted=True),
+            Position(guitar_string=4),
+            Position(guitar_string=3, is_highlighted=True),
+            Position(guitar_string=2),
+            Position(guitar_string=1, fret=3, finger=3, is_highlighted=True),
+        ]
+
+        self.shortened_shape = GuitarShape('G', 'Major', positions_without_string_5)
+
+        single_note = [
+            Position(guitar_string=6, fret=3, finger=2, is_highlighted=True),
+        ]
+
+        self.single_note_shape = GuitarShape('G', 'Major', single_note)
+
     def test_can_get_note_on_string_one(self):
         note = pick_pattern.get_guitar_string_note(self.six_string_shape, 1)
 
         self.assertEqual(note.guitar_string, 1)
 
     def test_can_get_note_on_string_two(self):
-        shape = g_major()
-
-        note = pick_pattern.get_guitar_string_note(shape, 2)
+        note = pick_pattern.get_guitar_string_note(self.six_string_shape, 2)
 
         self.assertEqual(note.guitar_string, 2)
 
     def test_can_get_note_on_string_three(self):
-        shape = g_major()
-
-        note = pick_pattern.get_guitar_string_note(shape, 3)
+        note = pick_pattern.get_guitar_string_note(self.six_string_shape, 3)
 
         self.assertEqual(note.guitar_string, 3)
 
     def test_can_get_note_on_string_four(self):
-        shape = g_major()
-
-        note = pick_pattern.get_guitar_string_note(shape, 4)
+        note = pick_pattern.get_guitar_string_note(self.six_string_shape, 4)
 
         self.assertEqual(note.guitar_string, 4)
 
     def test_can_get_note_on_string_five(self):
-        shape = g_major()
-
-        note = pick_pattern.get_guitar_string_note(shape, 5)
+        note = pick_pattern.get_guitar_string_note(self.six_string_shape, 5)
 
         self.assertEqual(note.guitar_string, 5)
 
     def test_can_get_note_on_string_six(self):
-        shape = g_major()
-
-        note = pick_pattern.get_guitar_string_note(shape, 6)
+        note = pick_pattern.get_guitar_string_note(self.six_string_shape, 6)
 
         self.assertEqual(note.guitar_string, 6)
 
     def test_throws_an_exception_for_an_invalid_string(self):
-        shape = g_major()
-
         with self.assertRaises(ValueError):
-            pick_pattern.get_guitar_string_note(shape, 7)
+            pick_pattern.get_guitar_string_note(self.six_string_shape, 7)
+
+    def test_get_root_note_returns_lowest_note(self):
+        note = pick_pattern.get_root_note(self.six_string_shape)
+
+        self.assertEqual(note.guitar_string, 6)
+
+    def test_get_alternate_root_note_returns_second_lowest_string(self):
+        note = pick_pattern.get_alternate_root_note(self.six_string_shape)
+
+        self.assertEqual(note.guitar_string, 5)
+
+    def test_get_alternate_root_note_adjusts_for_string_that_is_not_played(self):
+        note = pick_pattern.get_alternate_root_note(self.shortened_shape)
+
+        self.assertEqual(note.guitar_string, 4)
+
+    def test_get_alternate_root_note_raises_error_if_not_found(self):
+        with self.assertRaises(ValueError):
+            pick_pattern.get_alternate_root_note(self.single_note_shape)
