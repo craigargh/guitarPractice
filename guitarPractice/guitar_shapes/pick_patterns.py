925173b16a55c8de10089dd10895a79337cb70aa
Craig Richardson <craigargh@gmail.com>
2017-12-13 21:37:55 +0000

    Change difficulty of pick pattern collection one

--- guitarPractice/guitar_shapes/pick_patterns.py
+++ guitarPractice/guitar_shapes/pick_patterns.py
@@ -4,15 +4,21 @@ r = ROOT
 a = ALTERNATE_ROOT
 
 pattern_one = [r, 1, 2, 1, 2, 1, 2, 1]
-pattern_two = [r, 1, 2, 1, a, 1, 2, 1]
-pattern_three = [r, 2, a, 1, r, 2, a, 1]
-pattern_four = [r, 1, a, 2, r, 1, a, 2]
-pattern_five = [r, 3, 1, 2, r, 3, 1, 2]
-pattern_six = [r, 2, 1, 3, a, 2, 1, 3]
-pattern_seven = [r, 3, 2, 1, a, 3, 2, 1]
-pattern_eight = [r, 1, 2, 3, a, 1, 2, 3]
-pattern_nine = [r, 2, 3, 1, a, 2, 3, 1]
-pattern_ten = [r, 1, 3, 2, a, 1, 3, 2]
+pattern_two = [r, 3, 2, 1, r, 3, 2, 1]
+pattern_three = [r, 3, 1, 2, r, 3, 1, 2]
+pattern_four = [r, 2, 1, 3, r, 2, 1, 3]
+pattern_five = [r, 2, 3, 1, r, 2, 3, 1]
+pattern_six = [r, 1, 2, 3, r, 1, 2, 3]
+pattern_seven = [r, 1, 3, 2, r, 1, 3, 2]
+
+alternate_pattern_one = [r, 2, 1, 3, a, 2, 1, 3]
+alternate_pattern_two = [r, 3, 2, 1, a, 3, 2, 1]
+alternate_pattern_three = [r, 1, 2, 3, a, 1, 2, 3]
+alternate_pattern_four = [r, 2, 3, 1, a, 2, 3, 1]
+alternate_pattern_five = [r, 1, 3, 2, a, 1, 3, 2]
+alternate_pattern_size = [r, 1, a, 2, r, 1, a, 2]
+alternate_pattern_seven = [r, 1, 2, 1, a, 1, 2, 1]
+alternate_pattern_eight = [r, 2, a, 1, r, 2, a, 1]
 
 
 def pick_pattern_collection_one():
@@ -23,8 +29,18 @@ def pick_pattern_collection_one():
         pattern_four,
         pattern_five,
         pattern_six,
-        pattern_seven,
-        pattern_eight,
-        pattern_nine,
-        pattern_ten,
+        pattern_seven
+    ]
+
+
+def pick_pattern_collection_two():
+    return [
+        alternate_pattern_size,
+        alternate_pattern_one,
+        alternate_pattern_two,
+        alternate_pattern_three,
+        alternate_pattern_four,
+        alternate_pattern_five,
+        alternate_pattern_seven,
+        alternate_pattern_eight,
     ]
--- guitarPractice/tests/unit_tests/test_pick_patterns.py
+++ guitarPractice/tests/unit_tests/test_pick_patterns.py
@@ -10,51 +10,51 @@ class TestPickPatterns(TestCase):
         self.assertEqual(len(pattern), 8)
 
     def test_pattern_two_has_eight_notes(self):
-        pattern = pick_patterns.pattern_two
+        pattern = pick_patterns.alternate_pattern_seven
 
         self.assertEqual(len(pattern), 8)
 
     def test_pattern_three_has_eight_notes(self):
-        pattern = pick_patterns.pattern_three
+        pattern = pick_patterns.alternate_pattern_eight
 
         self.assertEqual(len(pattern), 8)
 
     def test_pattern_four_has_eight_notes(self):
-        pattern = pick_patterns.pattern_four
+        pattern = pick_patterns.alternate_pattern_size
 
         self.assertEqual(len(pattern), 8)
 
     def test_pattern_five_has_eight_notes(self):
-        pattern = pick_patterns.pattern_five
+        pattern = pick_patterns.pattern_three
 
         self.assertEqual(len(pattern), 8)
 
     def test_pattern_six_has_eight_notes(self):
-        pattern = pick_patterns.pattern_six
+        pattern = pick_patterns.alternate_pattern_one
 
         self.assertEqual(len(pattern), 8)
 
     def test_pattern_seven_has_eight_notes(self):
-        pattern = pick_patterns.pattern_seven
+        pattern = pick_patterns.alternate_pattern_two
 
         self.assertEqual(len(pattern), 8)
 
     def test_pattern_eight_has_eight_notes(self):
-        pattern = pick_patterns.pattern_eight
+        pattern = pick_patterns.alternate_pattern_three
 
         self.assertEqual(len(pattern), 8)
 
     def test_pattern_nine_has_eight_notes(self):
-        pattern = pick_patterns.pattern_nine
+        pattern = pick_patterns.alternate_pattern_four
 
         self.assertEqual(len(pattern), 8)
 
     def test_pattern_ten_has_eight_notes(self):
-        pattern = pick_patterns.pattern_ten
+        pattern = pick_patterns.alternate_pattern_five
 
         self.assertEqual(len(pattern), 8)
 
-    def test_collection_one_has_ten_patterns(self):
+    def test_collection_one_has_seven_patterns(self):
         collection = pick_patterns.pick_pattern_collection_one()
 
-        self.assertEqual(len(collection), 10)
+        self.assertEqual(len(collection), 7)
