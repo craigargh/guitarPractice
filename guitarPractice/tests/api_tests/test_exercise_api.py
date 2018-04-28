944ccf95b950e1dbe0d2973451be1ad2bcf9f389
Craig Richardson <craigargh@gmail.com>
2018-04-28 12:49:19 +0100

    Add API tests for rhythm

--- guitarPractice/tests/api_tests/test_exercise_api.py
+++ guitarPractice/tests/api_tests/test_exercise_api.py
@@ -9,6 +9,16 @@ class TestExerciseApi(TestCase):
     def setUp(self):
         self.api = app.test_client()
 
+    def test_invalid_exercise_name_returns_404(self):
+        response = self.api.get('/exercise/spooky-skeletons/1/')
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_invalid_exercise_name_returns_error_message(self):
+        response = self.api.get('/exercise/spooky-skeletons/1/')
+
+        self.assertEqual(response.data, b'Invalid exercise name or difficulty')
+
     def test_get_arpeggio_picking_one_exercise_returns_ok(self):
         response = self.api.get('/exercise/arpeggio-picking/1/')
 
@@ -26,63 +36,103 @@ class TestExerciseApi(TestCase):
 
         self.assertGreater(len(data['sequence']), 0)
 
-    def test_invalid_exercise_name_returns_404(self):
-        response = self.api.get('/exercise/spooky-skeletons/1/')
+    def test_get_arpeggio_picking_one_has_rhythm(self):
+        response = self.api.get('/exercise/arpeggio-picking/1/')
+        data = json.loads(response.data)
 
-        self.assertEqual(response.status_code, 404)
-
-    def test_invalid_exercise_name_returns_error_message(self):
-        response = self.api.get('/exercise/spooky-skeletons/1/')
-
-        self.assertEqual(response.data, b'Invalid exercise name or difficulty')
+        self.assertGreater(len(data['rhythm']), 0)
 
     def test_get_arpeggio_picking_two_exercise_returns_ok(self):
         response = self.api.get('/exercise/arpeggio-picking/2/')
 
         self.assertEqual(response.status_code, 200)
 
-    def test_get_arpeggio_picking_two_has_shapes_and_sequence(self):
+    def test_get_arpeggio_picking_two_has_shapes(self):
+        response = self.api.get('/exercise/arpeggio-picking/2/')
+        data = json.loads(response.data)
+
+        self.assertIsNotNone(data['shapes'])
+
+    def test_get_arpeggio_picking_two_has_sequence(self):
         response = self.api.get('/exercise/arpeggio-picking/2/')
         data = json.loads(response.data)
 
         self.assertIsNotNone(data['sequence'])
-        self.assertIsNotNone(data['shapes'])
+
+    def test_get_arpeggio_picking_two_has_rhythm(self):
+        response = self.api.get('/exercise/arpeggio-picking/2/')
+        data = json.loads(response.data)
+
+        self.assertIsNotNone(data['rhythm'])
 
     def test_get_chords_changes_one_exercise_returns_ok(self):
         response = self.api.get('/exercise/chord-changes/1/')
 
         self.assertEqual(response.status_code, 200)
 
-    def test_get_chords_changes_one_has_shapes_and_sequence(self):
+    def test_get_chords_changes_one_has_shapes(self):
+        response = self.api.get('/exercise/chord-changes/1/')
+        data = json.loads(response.data)
+
+        self.assertIsNotNone(data['shapes'])
+
+    def test_get_chords_changes_one_has_sequence(self):
         response = self.api.get('/exercise/chord-changes/1/')
         data = json.loads(response.data)
 
         self.assertIsNotNone(data['sequence'])
-        self.assertIsNotNone(data['shapes'])
+
+    def test_get_chords_changes_one_has_rhythm(self):
+        response = self.api.get('/exercise/chord-changes/1/')
+        data = json.loads(response.data)
+
+        self.assertIsNotNone(data['rhythm'])
 
     def test_get_major_scale_one_exercise_returns_ok(self):
         response = self.api.get('/exercise/major-scale/1/')
 
         self.assertEqual(response.status_code, 200)
 
-    def test_get_major_scale_one_has_shapes_and_sequence(self):
+    def test_get_major_scale_one_has_shapes(self):
+        response = self.api.get('/exercise/major-scale/1/')
+        data = json.loads(response.data)
+
+        self.assertIsNotNone(data['shapes'])
+
+    def test_get_major_scale_one_has_sequence(self):
         response = self.api.get('/exercise/major-scale/1/')
         data = json.loads(response.data)
 
         self.assertIsNotNone(data['sequence'])
-        self.assertIsNotNone(data['shapes'])
+
+    def test_get_major_scale_one_has_rhythm(self):
+        response = self.api.get('/exercise/major-scale/1/')
+        data = json.loads(response.data)
+
+        self.assertIsNotNone(data['rhythm'])
 
     def test_get_pentatonic_scale_one_exercise_returns_ok(self):
         response = self.api.get('/exercise/pentatonic-scale/1/')
 
         self.assertEqual(response.status_code, 200)
 
-    def test_get_pentatonic_scale_one_has_shapes_and_sequence(self):
+    def test_get_pentatonic_scale_one_has_shapes(self):
+        response = self.api.get('/exercise/pentatonic-scale/1/')
+        data = json.loads(response.data)
+
+        self.assertIsNotNone(data['shapes'])
+
+    def test_get_pentatonic_scale_one_has_sequence(self):
         response = self.api.get('/exercise/pentatonic-scale/1/')
         data = json.loads(response.data)
 
         self.assertIsNotNone(data['sequence'])
-        self.assertIsNotNone(data['shapes'])
+
+    def test_get_pentatonic_scale_one_has_rhythm(self):
+        response = self.api.get('/exercise/pentatonic-scale/1/')
+        data = json.loads(response.data)
+
+        self.assertIsNotNone(data['rhythm'])
 
     def test_get_exercises_returns_list_of_exercises(self):
         response = self.api.get('/exercises/')
